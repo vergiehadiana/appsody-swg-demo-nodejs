7a1105dea32408c9bd3fc25cd56a5fd8b35d95e8
vergiehadiana <43636831+vergiehadiana@users.noreply.github.com>
2020-04-24 21:04:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody - Version 1!");
+  res.send("Hello from Appsody - Version 01 !");
 });
  
 module.exports.app = app;
