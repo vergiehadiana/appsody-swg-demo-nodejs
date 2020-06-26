b3502e16e5252f3dd74430ba6a7a91614dca50fe
vergiehadiana <43636831+vergiehadiana@users.noreply.github.com>
2020-06-26 12:45:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from IBM Cloud Pak for Application! - SWG Vergie version 2");
+  res.send("Hello from IBM Cloud Pak for Application! - SWG Vergie version 1");
 });
  
 module.exports.app = app;2
